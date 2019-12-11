3b222dab58ca28c9cec6e1b82cf2fb7c0ad08303
Matt Alvarado <matimat2010@gmail.com>
2019-12-10 22:05:33 -0600

    updates to components

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = 'Cool Stuff Below';
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'http://mattalvarado.art/wp-content/uploads/2018/07/15587418856_5e16b0d009_z.jpg';
+  image3 = 'http://mattalvarado.art/wp-content/uploads/2018/07/15424933147_ced29cb6ec_z.jpg';
 
   constructor() { }
 
